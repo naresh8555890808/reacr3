cfda652fefa56c99b9c47ed7b9c75e3d4ccd463a
naresh8555890808 <141204145+naresh8555890808@users.noreply.github.com>
2023-09-09 19:52:06 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,57 @@
+class Bike extends React.Component {
+
+  constructor(props) {
+
+    super(props);
+
+     this.state = {
+
+      make: "Yamaha",
+
+      model: "R15",
+
+      color: "blue"
+
+    };
+
+  }
+
+  changeBikeColor = () => {
+
+    this.setState({color: "black"});
+
+  }
+
+  render() {
+
+    return (
+
+      <div>
+
+        <h2>My {this.state.make}</h2>
+
+        <p>
+
+          It is a {this.state.color}
+
+          {this.state.model}.
+
+        </p>
+
+        <button
+
+          type="button"
+
+          onClick={this.changeBikeColor}
+
+        >Change color</button>
+
+      </div>
+
+    );
+
+  }
+
+}
+
+
